af8690dcb79cde326a9d61a3dcbdc1434ff0c56b
Jakub Dwornik <jdwornik@prograils.com>
2022-07-29 19:02:58 +0200

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :rents
+  has_many :books, through: :rents
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
