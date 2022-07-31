42993b8c93adc85a0cd26189b5cd6a411f2f5af3
Jakub Dwornik <jdwornik@prograils.com>
2022-07-31 21:08:55 +0200

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def flash_class(flash)
+    if flash.alert
+      'alert-danger'
+    elsif flash.notice
+      'alert-success'
+    end
+  end
+
+  def flash_message(flash)
+    if flash.alert
+      flash.alert
+    elsif flash.notice
+      flash.notice
+    end
+  end
 end
