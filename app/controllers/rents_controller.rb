9f920ef561128bc53c46c3d885aa57ed422d6291
Jakub Dwornik <jdwornik@prograils.com>
2022-08-01 15:52:35 +0200

    Add rents authorization

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -37,7 +37,7 @@ class RentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_rent
-      @rent = Rent.find(params[:id])
+      @rent = Rent.where(user: current_user).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
