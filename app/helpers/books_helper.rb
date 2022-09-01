f798b7dc57bc33ca4f1e020bbda147fc78ee62b5
llkadenll <kdwornik@o2.pl>
2022-09-01 21:03:36 +0200

    Modify styling

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,10 @@
 module BooksHelper
   def rent_button(book)
-    button_to("Rent this book", rents_path(book_id: book), method: :post, class: 'btn btn-primary') if book.rents.empty? || book.rents.last.ended?
+    if book.rents.empty? || book.rents.last.ended?
+      button_to("Rent this book", rents_path(book_id: book), method: :post, class: 'btn btn-primary rent-button')
+    else
+      raw '<div class="btn btn-outline-secondary disabled rent-button">Unavailable</div>'
+    end
   end
 
   def book_status(book)
