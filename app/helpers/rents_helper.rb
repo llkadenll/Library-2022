5f21ca48276b96b86612237e002bc93ccdbb8cc5
llkadenll <kdwornik@o2.pl>
2022-09-02 21:38:11 +0200

    Modify rents styling

--- app/helpers/rents_helper.rb
+++ app/helpers/rents_helper.rb
@@ -1,2 +1,6 @@
 module RentsHelper
+  def return_button(rent)
+    return raw '<div class="btn btn-outline-secondary disabled rent-button">Returned</div>' unless rent.ongoing?
+    button_to("Return this book", return_book_path(rent), method: :patch, class: 'btn btn-primary return-button')
+  end
 end
